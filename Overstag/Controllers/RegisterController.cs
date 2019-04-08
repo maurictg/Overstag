f67a29876e85ca0ded01511300f7cc4eacd869be
MaurICT <31762519+maurictg@users.noreply.github.com>
2019-04-08 13:19:20 +0200

    Initial commit

---
+++ Overstag/Controllers/RegisterController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Overstag.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+
+namespace Overstag.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Login");
+        }
+
+        public IActionResult Login()
+        {
+            return View("Login");
+        }
+
+        public IActionResult Register()
+        {
+            return View("Register");
+        }
+
+        [HttpPost]
+        public ActionResult postRegister(Account account)
+        {
+            return Json(account);
+        }
+    }
+}
---
+++ Overstag/Models/DataModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.EntityFrameworkCore.Extensions;
+
+/// <summary>
+/// Database structuur
+/// </summary>
+namespace Overstag.Models
+{
+    //Tabellen maken
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+
+}
