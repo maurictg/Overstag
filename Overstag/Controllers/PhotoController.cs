9279926f5a116ef7486936c246e31add63b9d532
MaurICT <31762519+maurictg@users.noreply.github.com>
2019-06-25 11:47:45 +0200

    Just add a file

---
+++ Overstag/Controllers/PhotoController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Overstag.Controllers
+{
+    public class PhotoController
+    {
+        //Stuff here...
+    }
+}
