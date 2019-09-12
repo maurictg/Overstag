42e98b5c733bb2c10bf5a9fd7d040adada9e041d
MaurICT <31762519+maurictg@users.noreply.github.com>
2019-09-12 19:49:45 +0200

    Add API

---
+++ Overstag/Controllers/APIController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Overstag.Models;
+using Overstag.API;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Overstag.Controllers
+{
+    public class APIController : Controller
+    {
+        [HttpPost("API/Login")]
+        public JsonResult Login(LoginModel data)
+        {
+            using(var context = new OverstagContext())
+            {
+                try
+                {
+                    var account = context.Accounts.FirstOrDefault(f => f.Username == data.username || f.Email == data.username);
+
+                    if (account == null)
+                        return Json(new HttpObject { status = Status.error, message = "Gebruiker bestaat niet" });
+
+                    if (Encryption.PBKDF2.Verify(account.Password, data.password))
+                        return Json(new HttpObject { status = Status.success, data = new User { id = account.Id, email = account.Email, firstname = account.Firstname, lastname = account.Lastname, token = account.Token } });
+                    else
+                        return Json(new HttpObject { status = Status.error, message = "Wachtwoord is onjuist" });
+                }
+                catch(Exception e)
+                {
+                    return Json(new HttpObject { status = Status.error, message = e.Message, data = e });
+                }
+                
+            }
+        }
+
+
+    }
+}
---
+++ Overstag/Models/API.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Overstag.Models;
+
+namespace Overstag.API
+{
+    public enum Status { success, warning, error };
+
+    public class HttpObject
+    {
+        public Status status { get; set; }
+        public string message { get; set; }
+        public object data { get; set; }
+    }
+
+    public class Request
+    {
+        public string token { get; set; }
+    }
+
+    public class LoginModel
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+
+    public class Activity
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public DateTime when { get; set; }
+        public string description { get; set; }
+        public int cost { get; set; }
+        public bool subscribed { get; set; }
+    }
+
+    public class User
+    {
+        public int id { get; set; }
+        public string token { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string email { get; set; }
+    }
+
+
+}
