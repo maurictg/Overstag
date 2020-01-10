e8139fb97ac0e1b8171736da2767279cf4fb2ded
MaurICT <31762519+maurictg@users.noreply.github.com>
2020-01-10 13:53:59 +0100

    Add websockets

--- Overstag/Startup.cs
+++ Overstag/Startup.cs
@@ -1,16 +1,13 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using Overstag.Classes;
 using System.IO;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Razor.Runtime;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.FileProviders;
+using System.Net.WebSockets;
 
 namespace Overstag
 {
@@ -26,6 +23,7 @@ namespace Overstag
             services.AddDistributedMemoryCache(); // Adds a default in-memory implementation of IDistributedCache
             services.AddSession();
             services.AddResponseCaching();
+            services.AddRazorPages();
             services.AddResponseCompression(options => {
                 options.Providers.Add<GzipCompressionProvider>();
                 options.Providers.Add<BrotliCompressionProvider>();
@@ -51,10 +49,31 @@ namespace Overstag
             app.UseStaticFiles();
             app.UseMiddleware<Overstag.Middlewares.Authentication>();
             app.UseRouting();
+            app.UseWebSockets(new WebSocketOptions()
+            {
+                KeepAliveInterval = TimeSpan.FromSeconds(60),
+                ReceiveBufferSize = 1024 * 4
+            });
+
+            app.Use(async (context, next) => {
+                if (context.Request.Path == "/ws")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        WebSocket ws = await context.WebSockets.AcceptWebSocketAsync();
+                        await Sockets.HandleWS(context, ws);
+                    }
+                    else
+                        context.Response.StatusCode = 400;
+                }
+                else
+                    await next();
+            });
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapDefaultControllerRoute();
+                endpoints.MapRazorPages();
             });
         }
     }
